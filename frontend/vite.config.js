04251bdfebe2ab677acf3bbbde891765b6203f72
Harsh Paryani <harshparyani68@gmail.com>
2026-09-19 15:40:00 +0530

    chore: remove basicSsl from vite config for local testing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,5 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
-  server: {
-    https: true,
-  }
+  plugins: [react()]
 })
